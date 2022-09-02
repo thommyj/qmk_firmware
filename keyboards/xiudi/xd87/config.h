e32d151771cbffa8ec98321afd6716d7c1f79bbd
Thommy Jakobsson <thommyj@gmail.com>
2022-09-02 22:13:47 +0200

    Enable backlight on xd87
    Signed-off-by: Thommy Jakobsson <thommyj@gmail.com>

--- keyboards/xiudi/xd87/config.h
+++ keyboards/xiudi/xd87/config.h
@@ -47,7 +47,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define BACKLIGHT_PIN D0
 // #define BACKLIGHT_BREATHING
 #define BACKLIGHT_LEVELS 3
-#define BACKLIGHT_ON_STATE 0
+#define BACKLIGHT_ON_STATE 1
 
 #define RGB_DI_PIN B7
 #ifdef RGB_DI_PIN
