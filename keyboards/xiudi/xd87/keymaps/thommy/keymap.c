d1af062672374ee0c3ac828f86faa2d6da427cca
Thommy Jakobsson <thommyj@gmail.com>
2022-09-04 22:49:41 +0200

    Update xiudi:thommy layout and mapping
    enable split space, key next to escape, and keys next to arrow up.
    These all exists when using metal case
    
    Signed-off-by: Thommy Jakobsson <thommyj@gmail.com>

--- keyboards/xiudi/xd87/keymaps/thommy/keymap.c
+++ keyboards/xiudi/xd87/keymaps/thommy/keymap.c
@@ -16,20 +16,26 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_tkl_iso(
-      KC_ESC, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS,
-      KC_GRV, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP,
-      KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,            KC_DEL,  KC_END,  KC_PGDN,
-      KC_CAPS,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_NUHS, KC_ENT,
-      KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,
-      KC_LCTL,KC_LGUI,KC_LALT,                KC_SPC,                                 KC_RALT,MO(1), KC_APP,  KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT  ),
-  [1] = LAYOUT_tkl_iso(
-      KC_TRNS, KC_TRNS, KC_TRNS,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS,
-      KC_TRNS, KC_TRNS, KC_TRNS,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP,
-      KC_TRNS, RGB_TOG, RGB_VAI,   RGB_MOD,RGB_HUI,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,            KC_DEL,  KC_END,  KC_PGDN,
-      KC_TRNS, KC_TRNS, RGB_VAD,   RGB_RMOD,RGB_HUD,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT, KC_NUHS, KC_ENT,
-      KC_TRNS, KC_TRNS, KC_TRNS,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,            KC_UP,
-      KC_TRNS, KC_TRNS, KC_TRNS,                KC_SPC,                                 KC_RALT,MO(1), KC_APP,  KC_RCTL,   KC_LEFT, KC_DOWN, KC_RGHT  )
+  [0] = LAYOUT_tkl_metalcase_iso(
+      KC_ESC,   MO(2),    KC_F1,    KC_F2,    KC_F3,    KC_F4,         KC_F5,    KC_F6,    KC_F7,    KC_F8,         KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_PSCR,  KC_SLCK,  KC_PAUS,
+      KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,              KC_INS,   KC_HOME,  KC_PGUP,
+      KC_TAB,        KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                   KC_DEL,   KC_END,   KC_PGDN,
+      KC_CAPS,        KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
+      KC_LSFT,    KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,                      KC_UP,    KC_UP,    KC_UP,
+      KC_LCTL,     KC_LGUI,    KC_LALT,       KC_SPC,                                      KC_SPC,        KC_RALT,    MO(1),       KC_APP,     KC_RCTL,       KC_LEFT,  KC_DOWN,  KC_RGHT),
+  [1] = LAYOUT_tkl_metalcase_iso(
+      _______,  _______,  _______,  _______,  _______,  _______,       _______,  _______,  _______,  _______,       _______,  _______,  _______,  _______,    _______,  _______,  _______,
+      _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,  _______,               _______,  _______,  KC_AUDIO_VOL_UP,
+      _______,       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______, _______,  _______,                   _______,  _______,  KC_AUDIO_VOL_DOWN,
+      _______,        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+      _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,  KC_WREF,  _______,
+      _______,     _______,    _______,       KC_BSPC,                                     _______,       _______,    MO(1),       _______,    _______,       KC_WBAK,  _______,  KC_WFWD),
+  [2] = LAYOUT_tkl_metalcase_iso(
+      XXXXXXX,  MO(2),    RGB_TOG,  RGB_MOD,  RGB_RMOD, RGB_M_P,       RGB_M_B,  RGB_M_R,  RGB_M_SW, RGB_M_SN,      RGB_M_K,  RGB_M_X,  RGB_M_G,  RGB_M_T,    XXXXXXX,  XXXXXXX,  XXXXXXX,
+      XXXXXXX,  BL_TOGG,  BL_STEP,  BL_ON,    BL_OFF,   BL_BRTG,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  QK_BOOTLOADER,        XXXXXXX,  XXXXXXX,  BL_INC,
+      XXXXXXX,       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,                   XXXXXXX,  XXXXXXX,  BL_DEC,
+      XXXXXXX,        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+      XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                      RGB_SAI,  RGB_HUI,  RGB_VAI,
+      XXXXXXX,     XXXXXXX,    XXXXXXX,       XXXXXXX,                                     XXXXXXX,       XXXXXXX,    XXXXXXX,     XXXXXXX,    XXXXXXX,       RGB_SAD,  RGB_HUD,  RGB_VAD)
 };
 
-}
--- keyboards/xiudi/xd87/xd87.h
+++ keyboards/xiudi/xd87/xd87.h
@@ -67,3 +67,19 @@
     { k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4A, k4B, k4C, XXX, XXX, k4F, XXX }, \
     { k50, k51, k52, XXX, XXX, XXX, XXX, XXX, k58, XXX, k5A, k5B, k5C, k5D, k5E, k5F, k5G } \
 }
+
+#define LAYOUT_tkl_metalcase_iso( \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D,      k0E, k0F, k0G, \
+    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D,      k1E, k1F, k1G, \
+    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C,           k2E, k2F, k2G, \
+    k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D,                     \
+    k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4A, k4B, k4C,           k4E, k4F, k4G, \
+    k50, k51, k52,           k55, k58,                k5A, k5B, k5C, k5D,      k5E, k5F, k5G  \
+) { \
+    { k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k0A, k0B, k0C, k0D, k0E, k0F, k0G },  \
+    { k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k1A, k1B, k1C, k1D, k1E, k1F, k1G },  \
+    { k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k2A, k2B, k2C, XXX, k2E, k2F, k2G },  \
+    { k30, k31, k32, k33, k34, k35, k36, k37, k38, k39, k3A, k3B, k3C, k3D, XXX, XXX, XXX },  \
+    { k40, k41, k42, k43, k44, k45, k46, k47, k48, k49, k4A, k4B, k4C, XXX, k4E, k4F, k4G },  \
+    { k50, k51, k52, XXX, XXX, k55, XXX, XXX, k58, XXX, k5A, k5B, k5C, k5D, k5E, k5F, k5G }   \
+}
